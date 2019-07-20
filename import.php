9e6f34359efdc19e4b93bc330db8ff819777af30
ephestione <ephestione@gmail.com>
2019-07-20 20:17:35 +0200

    Update import.php

--- import.php
+++ import.php
@@ -2,9 +2,25 @@
 
 //importa i farmaci dall'elenco AIFA
 //formattare il CSV in modo che le prime tre colonne siano ATC,principio attivo,nome
-//disattivare la routine che richiede il carattere * nel nome del farmaco, per la lista dei farmaci in classe C
 function escapetrim($value) {
 	return  mysqli_real_escape_string($GLOBALS["dbcon"],trim($value));
+}
+
+$GLOBALS["dbcon"]=@mysqli_connect($dbhost, $dbuser, $dbpass);
+
+$row = 1;
+if (($handle = fopen("listefarmaci/a.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+		if (strpos($data[2],'*')!==false) {
+			$data[2] = substr($data[2], 0, strpos($data[2], '*'));
+			$data = array_map('escapetrim', $data);
+		}
+		else $data[2]="";
+		if ($data[0] && $data[1] && $data[2])
+			mysqli_query($GLOBALS["dbcon"],"INSERT IGNORE INTO farmaci (ATC, principio_attivo, nome) VALUES ('{$data[0]}','{$data[1]}','{$data[2]}')");
+    }
+    fclose($handle);
 }
 
 $row = 1;
@@ -17,7 +33,18 @@ if (($handle = fopen("listefarmaci/h.csv", "r")) !== FALSE) {
 		}
 		else $data[2]="";
 		if ($data[0] && $data[1] && $data[2])
-			doquery("INSERT IGNORE INTO farmaci (ATC, principio_attivo, nome) VALUES ('{$data[0]}','{$data[1]}','{$data[2]}')");
+			mysqli_query($GLOBALS["dbcon"],"INSERT IGNORE INTO farmaci (ATC, principio_attivo, nome) VALUES ('{$data[0]}','{$data[1]}','{$data[2]}')");
     }
     fclose($handle);
 }
+
+$row = 1;
+if (($handle = fopen("listefarmaci/c.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+		if ($data[0] && $data[1] && $data[2])
+			mysqli_query($GLOBALS["dbcon"],"INSERT IGNORE INTO farmaci (ATC, principio_attivo, nome) VALUES ('{$data[0]}','{$data[1]}','{$data[2]}')");
+    }
+    fclose($handle);
+}
+
